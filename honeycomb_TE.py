eb27b2fbc746c5f8b0f246cc5dccc5992420af29
mtthwprctr <mattp257@gmail.com>
2017-11-13 10:51:45 +0000

    beginning to clear up 'extended_cell.py'. Still need to create .py with common functions. Added honeycomb_TE.py for clarity.

--- honeycomb_TE.py
+++ honeycomb_TE.py
@@ -22,6 +22,25 @@ class Particle:
         self.loss = loss
 
 
+def honeycomb(spacing, radius, wp, loss):
+    """
+    Honeycomb lattice.
+
+    Create a honeycomb lattice with specific lattice spacing. Also define
+    radii, plasma frequency and loss of particles in the lattice.
+    """
+    particle_coords = []
+
+    particle_coords.append(Particle(spacing, 0, radius, wp, loss).R)
+    particle_coords.append(Particle(spacing*0.5, -spacing*np.sqrt(3)/2, radius, wp, loss).R)
+    particle_coords.append(Particle(-spacing*0.5, -spacing*np.sqrt(3)/2, radius, wp, loss).R)
+    particle_coords.append(Particle(-spacing, 0, radius, wp, loss).R)
+    particle_coords.append(Particle(-spacing*0.5, spacing*np.sqrt(3)/2, radius, wp, loss).R)
+    particle_coords.append(Particle(spacing*0.5, spacing*np.sqrt(3)/2, radius, wp, loss).R)
+
+    return np.array(particle_coords)
+
+
 def honeycomb_reciprocal_space(spacing, resolution):
     """
     Create set of (x,y) coordinates for path in reciprocal space.
@@ -44,34 +63,31 @@ def honeycomb_reciprocal_space(spacing, resolution):
 def green(k, distance):
     """
     Green's function interaction.
+
+    Calculate the Green's function tensor between particles which are separated
+    by a vector distance at a frequency k. For a 2D Green's function, the
+    interactions are modelled with Hankel function.s
+
+    Returns a matrix of the form [[G_xx, G_xy],[G_xy, G_yy]]
     """
     x = distance[0]
     y = distance[1]
     R = np.linalg.norm(distance)
     arg = k*R
 
-    result = 0.25j * k**2 * sp.special.hankel1(0, arg)
+    xx_type = 0.25j * k**2 * (
+    (y**2/R**2) * sp.special.hankel1(0,arg) +
+    (x**2 - y**2)/(k*R**3) * sp.special.hankel1(1, arg)
+    )
 
-    return result
+    yy_type = 0.25j * k**2 * (
+    (x**2/R**2) * sp.special.hankel1(0,arg) -
+    (x**2 - y**2)/(k*R**3) * sp.special.hankel1(1, arg)
+    )
 
+    xy_type = 0.25j * k**2 * x*y/R**2 * sp.special.hankel1(2,arg)
 
-def honeycomb(spacing, radius, wp, loss):
-    """
-    Honeycomb lattice.
-
-    Create a honeycomb lattice with specific lattice spacing. Also define
-    radii, plasma frequency and loss of particles in the lattice.
-    """
-    particle_coords = []
-
-    particle_coords.append(Particle(spacing, 0, radius, wp, loss).R)
-    particle_coords.append(Particle(spacing*0.5, -spacing*np.sqrt(3)/2, radius, wp, loss).R)
-    particle_coords.append(Particle(-spacing*0.5, -spacing*np.sqrt(3)/2, radius, wp, loss).R)
-    particle_coords.append(Particle(-spacing, 0, radius, wp, loss).R)
-    particle_coords.append(Particle(-spacing*0.5, spacing*np.sqrt(3)/2, radius, wp, loss).R)
-    particle_coords.append(Particle(spacing*0.5, spacing*np.sqrt(3)/2, radius, wp, loss).R)
-
-    return np.array(particle_coords)
+    return np.array([[xx_type, xy_type], [xy_type, yy_type]])
 
 
 def honeycomb_supercell(t1, t2, max):
@@ -96,31 +112,49 @@ def honeycomb_supercell(t1, t2, max):
 
 
 def interactions(intracell, intercell, w, q):
-    H = np.zeros((6, 6), dtype=np.complex_)
+    """
+    Interaction matrix.
+
+    Find the matrix of interactions for a 6 site unit cell system. Interactions
+    are both between particle in the same cell (INTRAcell) and between
+    particles in different cells (INTERcell). Calculated at a particular
+    frequency omega (w) and Bloch wavenumber (q).
+    """
+    H = np.zeros((12, 12), dtype=np.complex_)
+
     k = w*ev
+
+    # We only need to fill 'top right' diagonal of the matrix since 'opposite
+    # direction' interactions are given by the Hermitian conjugate.
 
     indices = np.arange(len(intracell))
     for n, m in itertools.combinations(indices, 2):
-        H[n][m] = sum([green(k, -intracell[n] + intracell[m] + inter) * np.exp(1j * np.dot(q, -intracell[n] + intracell[m] + inter)) for inter in intercell])
-        H[m][n] = sum([green(k, -(-intracell[n] + intracell[m] + inter)) * np.exp(1j * np.dot(q, -(-intracell[n] + intracell[m] +  inter))) for inter in intercell])
+        H[2*n:2*n+2, 2*m:2*m+2] = sum([green(k, -intracell[n] + intracell[m] + inter) * np.exp(1j * np.dot(q, -intracell[n] + intracell[m] + inter)) for inter in intercell])
+        H[2*m:2*m+2, 2*n:2*n+2] = sum([green(k, -(-intracell[n] + intracell[m] + inter)) * np.exp(1j * np.dot(q, -(-intracell[n] + intracell[m] +  inter))) for inter in intercell])
 
+
+    # Create the diagonal by considering interactions between same particle
+    # sites but in different cells. Need to make sure to ignore the (0,0)
+    # element in intercell to prevent issues with the Green's function as we
+    # have no 'self interaction'.
     for n in np.arange(len(intracell)):
         to_sum = []
         for inter in intercell:
             if np.linalg.norm(inter) != 0:  # ignore (0,0) position
                 to_sum.append(green(k, inter) * np.exp(1j * np.dot(q, inter)))
-        H[n][n] = sum(to_sum)
-
+        H[2*n:2*n+2, 2*n:2*n+2] = sum(to_sum)
     return H
 
 
 def polar(w, wp, loss, radius):
+    """Polarisability function.
+
+    Dynamic polarisability for infinite cylinder.
+    """
     k = w*ev
     eps = 1 - (wp**2)/(w**2 + 1j*loss*w)
-    static = np.pi*radius**2 * (eps - 1)
-    result = static/(1 - 0.25j * np.pi * (k*radius)**2 * (eps-1))
-
-    return result
+    static = 2 * np.pi * radius**2 * ((eps-1)/(eps+1))
+    return static/(1 - 0.25j * (radius**2 * k**2) * ((eps-1)/(eps+1)))
 
 
 def extinction(w, q, intracell, intercell):
@@ -149,12 +183,23 @@ def calculate_extinction(wrange, qrange, intracell, intercell):
     return results
 
 
+def plot_extinction(wrange, qrange, intracell, intercell, t1, t2, resolution):
+    light_line = [(np.linalg.norm(qval)/ev) for q, qval in enumerate(qrange)]
+    plt.plot(light_line, 'r--', zorder = 1, alpha = 0.5)
+
+    raw_results = calculate_extinction(wrange, qrange, intracell, intercell)
+    reshaped_results = np.array(raw_results).reshape((resolution, resolution))
+    plt.imshow(reshaped_results, origin='lower', extent=[0, resolution-1, wmin, wmax], aspect='auto', cmap='viridis', zorder=0)
+
+    plt.show()
+
+
 if __name__ == "__main__":
     a = 15.*10**-9  # lattice spacing
     r = 5.*10**-9  # particle radius
     wp = 6.18  # plasma frequency
-    g = 0.08  # losses
-    scaling = 1.
+    g = 0.0  # losses
+    scaling = 1.05
     ev = (1.602*10**-19 * 2 * np.pi)/(6.626*10**-34 * 2.997*10**8)  # k-> w conversion
     c = 2.997*10**8  # speed of light
 
@@ -163,17 +208,12 @@ if __name__ == "__main__":
     intracell = honeycomb(a, r, wp, g)
     intercell = honeycomb_supercell(trans_1, trans_2, 1)
 
-    wmin = 2
-    wmax = 3
+    wmin = wp/np.sqrt(2) - 0.5
+    wmax = wp/np.sqrt(2) + 0.5
 
     resolution = 100
 
     wrange = np.linspace(wmin, wmax, resolution, endpoint=True)
     qrange = honeycomb_reciprocal_space(a, resolution)
 
-    light_line = [(np.linalg.norm(qval)/ev) for q, qval in enumerate(qrange)]
-    plt.plot(light_line, zorder=1)
-    raw_results = calculate_extinction(wrange, qrange, intracell, intercell)
-    reshaped_results = np.array(raw_results).reshape((resolution, resolution))
-    plt.imshow(reshaped_results, origin='lower', extent=[0, resolution-1, wmin, wmax], aspect='auto', cmap='viridis', zorder=0)
-    plt.show()
+    plot_extinction(wrange, qrange, intracell, intercell, trans_1, trans_2, resolution)
